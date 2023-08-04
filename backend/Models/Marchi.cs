fca3e2b23b20543725cbc98d524e288fa5790482
Yalian Rodriguez - ICOM Srl <yrodriguez@icomservice.it>
2023-08-04 10:44:43 +0200

    refactor: server side code first (marchi modelli) entities

---
+++ backend/Models/Marchi.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Motostore.Models
+{
+    public class Marchi
+    {
+        [Key]
+        public string Marchio { get; set; }
+        public virtual ICollection<Modelli> Modelli { get; set; }
+
+        public Marchi()
+        {
+            Marchio = string.Empty;
+            Modelli = new List<Modelli>();
+        }
+    }
+}
